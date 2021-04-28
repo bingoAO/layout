c14117127f48712082a4af0b83554168d72514ac
sheshihao <sheshihao@mbcloud.com>
2021-04-28 10:24:13 +0800

    feat:init project

---
+++ .history/src/App_20210428101236.js
@@ -0,0 +1,44 @@
+import './App.css';
+import {Table} from 'antd';
+
+function App() {
+  const dataSource = [
+    {
+      key: '1',
+      name: '胡彦斌',
+      age: 32,
+      address: '西湖区湖底公园1号',
+    },
+    {
+      key: '2',
+      name: '胡彦祖',
+      age: 42,
+      address: '西湖区湖底公园1号',
+    },
+  ];
+  
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+      with:100
+    },
+    {
+      title: '年龄',
+      dataIndex: 'age',
+      key: 'age',
+      with:200
+    },
+    {
+      title: '住址',
+      dataIndex: 'address',
+      key: 'address',
+      with:300
+    },
+  ];
+  
+  return <div className='parent'> <Table dataSource={dataSource} columns={columns} /> </div>;
+}
+
+export default App;
